9578f514edea309c1265fd4c8753b2047a625a4c
Holmes-EH <holmes.samuel@yahoo.fr>
2022-01-05 20:19:13 +0100

    clearup Component names

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,37 @@
 import Head from 'next/head'
 import styles from '@/styles/Containers.module.css'
-import Post from '@/components/Post'
+import Link from 'next/link'
+import Card from '@/components/Card'
 import posts from 'postData'
 
 const Home = () => {
 	return (
-		<div className={styles.container}>
+		<>
 			<Head>
 				<title>CMS</title>
 				<meta name='description' content='CMS Boilerplate' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 
-			<main className={styles.main}>
-				<h1 className={styles.title}>Welcome</h1>
+			<h1 className={styles.title}>Welcome</h1>
 
-				{posts.map((post) => {
-					return (
-						<div key={post.time} className={styles.post}>
-							<Post blocks={post.blocks} />
-						</div>
-					)
-				})}
-			</main>
-
-			<footer className={styles.footer}>Footer</footer>
-		</div>
+			{posts.map((post) => {
+				return (
+					<div key={post.time} className={styles.post}>
+						<Link href={`/posts/${post.slug}`} passHref>
+							<a>
+								<Card
+									title={post.blocks[0].data.text}
+									body='Lorem ipsum dolor sit amet consectetur adipisicing elit. (...)'
+									createdAt='2021-11-26'
+									postDate='2021-11-26'
+								/>
+							</a>
+						</Link>
+					</div>
+				)
+			})}
+		</>
 	)
 }
 
