bdfed1f3a21f053115da6b8355b51693f54d93d6
Holmes-EH <holmes.samuel@yahoo.fr>
2022-01-05 11:26:53 +0100

    add svgr

--- next.config.js
+++ next.config.js
@@ -4,4 +4,16 @@ module.exports = {
 		locales: ['fr'],
 		defaultLocale: 'fr',
 	},
+	images: {
+		domains: ['cdn.pixabay.com'],
+	},
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/i,
+			issuer: /\.[jt]sx?$/,
+			use: ['@svgr/webpack'],
+		})
+
+		return config
+	},
 }
