4ff710509c54c7ac15acccefff3ffcda6ff17e7b
Holmes-EH <holmes.samuel@yahoo.fr>
2022-01-22 10:44:20 +0100

    animate pages changes

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,53 @@
 import Navbar from './Navbar'
 import styles from '@/styles/Containers.module.css'
+import { motion, AnimatePresence } from 'framer-motion'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 
 const Layout = ({ children }) => {
+	const router = useRouter()
+	const [curUrl, setCurUrl] = useState('')
+	const [translateOnExitValue, setTranslateOnExitValue] = useState(-10)
+	useEffect(() => {
+		setCurUrl(router.asPath)
+	})
+
+	useEffect(() => {
+		const handleRouteChange = (url) => {
+			url.length > curUrl.length
+				? setTranslateOnExitValue(-20)
+				: setTranslateOnExitValue(20)
+		}
+
+		router.events.on('routeChangeStart', handleRouteChange)
+
+		// If the component is unmounted, unsubscribe
+		// from the event with the `off` method:
+		return () => {
+			router.events.off('routeChangeStart', handleRouteChange)
+		}
+	}, [curUrl])
+
 	return (
 		<div className={styles.container}>
 			<Navbar />
-			<main className={styles.main}>{children}</main>
+
+			<AnimatePresence exitBeforeEnter>
+				<motion.main
+					className={styles.main}
+					key={router.pathname}
+					initial={{
+						opacity: 0,
+					}}
+					animate={{
+						opacity: 1,
+					}}
+					exit={{ opacity: 0, x: translateOnExitValue }}
+					transition={{ duration: 0.3 }}
+				>
+					{children}
+				</motion.main>
+			</AnimatePresence>
 			<footer className={styles.footer}>Footer</footer>
 		</div>
 	)
--- pages/404.js
+++ pages/404.js
@@ -1,20 +1,55 @@
 import styles from '@/styles/404.module.css'
 import Link from 'next/link'
+import { motion } from 'framer-motion'
 
 const Custom404 = () => {
+	const container = {
+		hidden: { opacity: 0 },
+		show: {
+			opacity: 1,
+			transition: {
+				staggerChildren: 0.1,
+				delayChildren: 0.3,
+			},
+		},
+	}
+	const item = {
+		hidden: { opacity: 0, y: -15, rotateY: 90 },
+		show: { opacity: 1, y: 0, rotateY: 0 },
+	}
 	return (
 		<>
-			<div className={styles.numbers}>
-				<div>4</div>
-				<div>0</div>
-				<div>4</div>
-			</div>
-			<h2>The page you asked for cannot be found !</h2>
-			<Link href='/' passRef>
-				<h3 className={styles.explanation}>
-					<a>Go to Home Page</a>
-				</h3>
-			</Link>
+			<motion.div
+				className={styles.numbers}
+				variants={container}
+				initial='hidden'
+				animate='show'
+				transition={{
+					type: 'spring',
+				}}
+			>
+				<motion.div variants={item}>4</motion.div>
+				<motion.div variants={item}>0</motion.div>
+				<motion.div variants={item}>4</motion.div>
+			</motion.div>
+			<motion.h2
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				transition={{ delay: 0.6 }}
+			>
+				The page you asked for cannot be found !
+			</motion.h2>
+			<motion.div
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				transition={{ delay: 1, duration: 0.5 }}
+			>
+				<Link href='/' passRef>
+					<h3 className={styles.explanation}>
+						<a>Go to Home Page</a>
+					</h3>
+				</Link>
+			</motion.div>
 		</>
 	)
 }
