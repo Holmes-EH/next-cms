fd8e4aa274ab92bcaec16223b6d5e85fde481f47
Holmes-EH <holmes.samuel@yahoo.fr>
2022-01-22 19:44:48 +0100

    add hero image

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import styles from '@/styles/Containers.module.css'
 import Link from 'next/link'
 import Card from '@/components/Card'
 import posts from 'postData'
+import Image from 'next/image'
 
 const Home = () => {
 	return (
@@ -13,13 +14,19 @@ const Home = () => {
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 
-			<h1 className={styles.title}>Welcome</h1>
+			<div className={styles.hero}>
+				<Image
+					src='https://cdn.pixabay.com/photo/2021/12/15/17/16/technology-6873012_1280.jpg'
+					layout='fill'
+					objectFit='cover'
+				/>
+			</div>
 
 			{posts.map((post) => {
 				return (
 					<div key={post.time} className={styles.post}>
 						<Link href={`/posts/${post.slug}`} passHref>
-							<a>
+							<a style={{ textDecoration: 'none' }}>
 								<Card
 									title={post.blocks[0].data.text}
 									body='Lorem ipsum dolor sit amet consectetur adipisicing elit. (...)'
