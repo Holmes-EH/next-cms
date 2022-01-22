f198c2be500d35f82f0f4591458e369baa7a275c
Holmes-EH <holmes.samuel@yahoo.fr>
2022-01-22 19:44:25 +0100

    add viewport meta to _app head

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,20 @@
+import Head from 'next/head'
 import Layout from '@/components/Layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+		<>
+			<Head>
+				<meta
+					name='viewport'
+					content='minimum-scale=1, initial-scale=1,maximum-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover'
+				/>
+			</Head>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</>
 	)
 }
 
